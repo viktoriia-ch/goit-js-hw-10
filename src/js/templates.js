c5e1729e1c818d6373dcc9eb9113be4f758880d2
Viktoriia Chumak <vikaff17@gmail.com>
2022-12-01 16:44:20 +0200

    changes in markup

--- src/js/templates.js
+++ src/js/templates.js
@@ -14,7 +14,13 @@ export const countryTemplate = ({
   return `<div class='country-container-heading'>
   <img src=${flags.svg} width='60px' height="40px"/>
   <h2 class='country-container-title'>${name.official}</h2></div>
-  <p><span class='country-container-key'>Capital: </span>${capital}</p>
-  <p><span class='country-container-key'>Population: </span>${population}</p>
-  <p><span class='country-container-key'>Languages: </span>${languagesList}</p>`;
+  <ul class='country-container-list'>
+  <li><span class='country-container-item'>Capital: </span>${capital}</li>
+  <li><span class='country-container-item'>Population: </span>${population}</li>
+  <li><span class='country-container-item'>Languages:</span>${languagesList}</li>
+  </ul>`;
 };
+
+//  <p><span class='country-container-key'>Capital: </span>${capital}</p>
+// <p><span class='country-container-key'>Population: </span>${population}</p>
+// <p><span class='country-container-key'>Languages: </span>${languagesList}</p>`;
