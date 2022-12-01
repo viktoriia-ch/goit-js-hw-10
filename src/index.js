610d0d67ec7923307da3005e1b3150c038908e92
Viktoriia Chumak <vikaff17@gmail.com>
2022-12-01 16:20:47 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,90 @@
+// IMPORTS
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { itemTemplate, countryTemplate } from './js/templates';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  container: document.querySelector('.country-info'),
+};
+
+// FUNCTIONS
+const clearList = () => {
+  refs.list.innerHTML = '';
+};
+
+const clearContainer = () => {
+  refs.container.innerHTML = '';
+};
+
+const renderList = countries => {
+  const country = countries.map(itemTemplate).join('');
+  refs.list.insertAdjacentHTML('beforeend', country);
+};
+
+const renderContainer = countries => {
+  if (refs.container.innerHTML === '') {
+    const country = countryTemplate(countries[0]);
+    refs.container.insertAdjacentHTML('beforeend', country);
+  }
+};
+
+const onError = () => {
+  clearList();
+  clearContainer();
+  Notify.failure('Oops, there is no country with that name');
+};
+
+const showInfoNotification = () => {
+  clearList();
+  clearContainer();
+  Notify.info('Too many matches found. Please enter a more specific name.');
+};
+
+const showCountriesList = countries => {
+  clearContainer();
+  renderList(countries);
+};
+
+const showCountryInfo = countries => {
+  clearList();
+  renderContainer(countries);
+};
+
+const showCountries = countries => {
+  if (countries.length > 10) {
+    showInfoNotification();
+  }
+
+  if (countries.length >= 2 && countries.length < 10) {
+    showCountriesList(countries);
+  }
+
+  if (countries.length === 1) {
+    showCountryInfo(countries);
+  }
+};
+
+const onInputName = e => {
+  const { value } = e.target;
+  if (value.trim() === '') {
+    clearList();
+    clearContainer();
+  }
+
+  fetchCountries(value)
+    .then(response => response.json())
+    .then(countries => {
+      if (countries.status === 404) {
+        throw new Error();
+      }
+      showCountries(countries);
+    })
+    .catch(onError);
+};
+
+refs.input.addEventListener('input', debounce(onInputName, DEBOUNCE_DELAY));
---
+++ src/js/templates.js
@@ -0,0 +1,20 @@
+export const itemTemplate = ({ name, flags }) => `<li class='country-list-item'>
+  <img class='country-list-img' src=${flags.svg} width='20px' height='15px'>
+  <span'>${name.official}</span></li>`;
+
+export const countryTemplate = ({
+  name,
+  capital,
+  population,
+  flags,
+  languages,
+}) => {
+  const languagesList = Object.values(languages).join(', ');
+
+  return `<div class='country-container-heading'>
+  <img src=${flags.svg} width='60px' height="40px"/>
+  <h2 class='country-container-title'>${name.official}</h2></div>
+  <p><span class='country-container-key'>Capital: </span>${capital}</p>
+  <p><span class='country-container-key'>Population: </span>${population}</p>
+  <p><span class='country-container-key'>Languages: </span>${languagesList}</p>`;
+};
